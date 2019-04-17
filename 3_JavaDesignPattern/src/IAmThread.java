14df0b38b969982a705900b07b89c6efed9c656e
Jack_PengPeng <zzhuopeng@gmail.com>
2019-04-17 17:11:08 +0800

    在原来空文件夹加上了暂时无关的文件，为了提交空文件夹

---
+++ 3_JavaDesignPattern/src/IAmThread.java
@@ -0,0 +1,25 @@
+public class IAmThread {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
+
+class MyThread extends Thread {
+
+    int timer = 0;
+
+    @Override
+    public void run() {
+
+        while (true){
+            System.out.println("I am Thread Timer , now is " + timer++);
+
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
