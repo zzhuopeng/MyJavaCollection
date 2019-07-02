9c71627b5a5cac1dff72a5235035c04afa5c44a9
Jack_PengPeng <zzhuopeng@gmail.com>
2019-07-02 20:48:26 +0800

    提交一下，好像没啥更新的

--- JDKReader/src/jdk1_8_0_131/java/util/HashMap.java
+++ JDKReader/src/jdk1_8_0_131/java/util/HashMap.java
@@ -139,7 +139,7 @@ public class HashMap<K,V> extends AbstractMap<K,V>
 
     private static final long serialVersionUID = 362498820763181265L;
 
-    /*
+    /**
      * Implementation notes.
      *
      * This map usually acts as a binned (bucketed) hash table, but
